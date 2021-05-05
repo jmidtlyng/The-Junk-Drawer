b6d4a8060ca989496f65567bed27783e99bdeecb
Jacob Midtlyng <jacobmidtlyng@Jacobs-MacBook-Pro.local>
2021-05-05 00:29:14 -0700

    homing in on auto-routing using quote

--- core/src/routes.rs
+++ core/src/routes.rs
@@ -1,10 +1,24 @@
 use actix_web::web;
+use glob::glob;
+use std::str;
+use quote::quote;
 use crate::views;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
-    let admin_scope = web::scope("/admin")
-        .service(views::admin::get)
-        .service(views::admin::entities::get);
+    let admin_scope = web::scope("/admin");
+    
+    for template in glob("views/admin/**").expect("Failed to read entry path"){
+        match template {
+            Ok(template_pathbuf) => {                    
+                let template_str = template_pathbuf.to_str().unwrap();
+                let template_path = str::replace(template_str, "/", "::");
+                let template_service = quote! { #template_path };
+                        
+                admin_scope.service( template_service.into() );
+            },
+            Err(e) => println! ("{:?}", e)
+        }
+    }
     
     cfg.service( admin_scope );
 }
